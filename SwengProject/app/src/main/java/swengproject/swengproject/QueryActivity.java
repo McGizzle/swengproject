2ba5960b0f87aed023c5e4dc48c1363c49d2ecaa
Mark Murtagh <mamurtag@tcd.ie>
2016-11-15 16:11:16 +0000

    merchants design take2

--- SwengProject/app/src/main/java/swengproject/swengproject/QueryActivity.java
+++ SwengProject/app/src/main/java/swengproject/swengproject/QueryActivity.java
@@ -2,49 +2,76 @@ package swengproject.swengproject;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.ArrayList;
 
 /**
  * Created by McGroarty on 08/11/16.
  */
 public class QueryActivity extends AppCompatActivity {
     final String SERVER_URL = "SERVER URL";
+    private ProgressBar pb;
+    private Button btn;
+    ArrayList<String> DATA;
+    ArrayList<String> META_DATA;
+    private String response;
+
 
     @Override
     protected void onCreate(final Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_query);                //Sets the XML file
 
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitNetwork().build();
-        StrictMode.setThreadPolicy(policy);
+
+      //  StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitNetwork().build();
+      //  StrictMode.setThreadPolicy(policy);
+
 
         Bundle extras = getIntent().getExtras();                //How to get Data from previous activity
-        String data = extras.getString("DATA NAME");
-        String data2 = extras.getString("DATA2 NAME");
+        String view = extras.getString("VIEW");
+        setContentView(R.layout.view);
+        DATA  = extras.getStringArrayList("DATA");
+        META_DATA = extras.getStringArrayList("META_DATA");
+
+        pb = (ProgressBar)findViewById(R.id.progressBar1);
+        pb.setVisibility(View.VISIBLE);
+        new MyAsyncTask().execute();
 
     }
+
     /*
     * insertMySQLPost()
-    * Params: None
-    * Description: Function appends data to URL and attempts to POST data to PHP script
-    * Return: Boolean: succesfully entered data
+    * Params: ArrayList<String> = Data
+    *         ArrayList<String> = Meta
+    * Description: Function appends data to URL and attempts to POST data to PHP script. Also receives response.
+    * Return: String - Response from server; NULL if unsuccessfull
     */
 
-    public boolean insertMySQLPost() throws IOException {
+    public String insertMySQLPost() throws IOException {
 
-        String DATA_VARIABLE = "";
-        String data = URLEncoder.encode("DATA NAME", "UTF-8")
-                + "=" + URLEncoder.encode(DATA_VARIABLE, "UTF-8");
+        StringBuilder result = new StringBuilder();
+
+        for(int i=0;i<META_DATA.size()&&i<DATA.size();i++) {
+            String x = URLEncoder.encode(META_DATA.get(i), "UTF-8")
+                    + "=" + URLEncoder.encode(DATA.get(i), "UTF-8");
+            result.append(x);
+        }
 
         // Send data
         try {
@@ -54,22 +81,32 @@ public class QueryActivity extends AppCompatActivity {
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setDoOutput(true);
             OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
-            wr.write(data);
+            wr.write(result.toString());
             wr.flush();
+
             if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
                 Log.d("Tag", "Success");
+                InputStream in = new BufferedInputStream(conn.getInputStream());
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    result.append(line);
+                }
+                insertSuccess();
+
             } else {
                 Log.d("Tag", "Failure");
-                return false;
+                insertFail();
+                return null;
             }
 
         }catch(Exception ex) {
             ex.printStackTrace();
             Log.d("Tag", "Failure");
-            return false;
+            return null;
         }
-
-        return true;
+        response = result.toString();
+        return result.toString();
     }
 
     /* insertSuccess()
@@ -85,7 +122,6 @@ public class QueryActivity extends AppCompatActivity {
         Toast toast = Toast.makeText(context, text, duration);
         toast.setMargin(toast.getHorizontalMargin() / 2, toast.getVerticalMargin() / 2);
         toast.show();
-        setContentView(R.layout.end_activity);
     }
 
     /* insertFail()
@@ -100,9 +136,27 @@ public class QueryActivity extends AppCompatActivity {
         Toast toast = Toast.makeText(context, text, duration);
         toast.setMargin(toast.getHorizontalMargin() / 2, toast.getVerticalMargin() / 2);
         toast.show();
-        Intent i = new Intent(QueryActivity.this, MainActivity.class);
-        startActivity(i);
     }
+
+    private class MyAsyncTask extends AsyncTask<String, Integer, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                insertMySQLPost();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        protected void onPostExecute(){
+            pb.setVisibility(View.GONE);
+            Toast.makeText(getApplicationContext(), "command sent", Toast.LENGTH_LONG).show();
+        }
+        protected void onProgressUpdate(Integer... progress){
+            pb.setProgress(progress[0]);
+        }
 
 
 }
--- insert.php
+++ insert.php
@@ -1,16 +1,61 @@
 <?php
 
-	ini_set('display_errors', 1); 
+	ini_set('display_errors', 1);
 	error_reporting(E_ALL);
 
-	$host="mysql2772int.cp.blacknight.com"; 
+	$host="mysql2772int.cp.blacknight.com";
 	$uname="u1429550_sweng";
 	$pwd="sw3ngproject?";
 	$db="db1429550_swengproject";
 
 	$con = mysqli_connect($host,$uname,$pwd,$db) or die("connection failed");
 
-	//Querys go here
+	$type = $_POST["type"];
+
+	switch(type){
+		case 1: add_project();
+		break;
+		case 2: add_individuals();
+		break;
+		case 3: attach_individuals();
+		break;
+		case 4: insert_object();
+		break;
+		case 5: assign_object();
+		break;
+		case 6: date_objects_list();
+		break;
+		case 7: other_objects_list();
+		break;
+		case 8: find_object();
+		break;
+	}
+
+	function add_project(){
+
+	}
+	function add_individuals(){
+
+	}
+	function attach_individuals(){
+
+	}
+	function insert_object(){
+
+	}
+	function assign_object(){
+
+	}
+	function date_objects_list(){
+
+	}
+	function other_objects_list(){
+
+	}
+	function find_object(){
+		
+	}
+
 
 	mysql_close($con);
 ?>
