e1c09644f261f8d53e35a0720688633c2401e417
Sean McGroarty <mcgroas@tcd.ie>
2016-12-02 15:58:32 +0000

    ListActivity

--- SwengProject/app/src/main/java/swengproject/swengproject/ListActivity.java
+++ SwengProject/app/src/main/java/swengproject/swengproject/ListActivity.java
@@ -12,32 +12,41 @@ import android.widget.Button;
 public class ListActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(final Bundle savedInstanceState){
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(list_options);
 
         Button reclaimBttn = (Button) findViewById(R.id.reclaimBttn);
-        Button attachedBttn  = (Button) findViewById(R.id.attachedBttn);
+        Button attachedBttn = (Button) findViewById(R.id.attachedBttn);
         Button brokenBttn = (Button) findViewById(R.id.brokenBttn);
         reclaimBttn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                reclaimed();
             }
         });
         attachedBttn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                attached();
             }
         });
         brokenBttn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                broken();
             }
         });
 
+    }
+    public void reclaimed(){
+        //TODO
+    }
+    public void attached(){
+        //TODO
+    }
+    public void broken(){
+        //TODO
     }
 
 }
