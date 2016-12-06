f4347d925eab8474bb701c14b90daa12cc660e90
Mark Murtagh <mamurtag@tcd.ie>
2016-12-06 15:12:05 +0000

    marks changes

--- SwengProject/app/src/main/java/swengproject/swengproject/AssignObjectActivity.java
+++ SwengProject/app/src/main/java/swengproject/swengproject/AssignObjectActivity.java
@@ -1,14 +1,19 @@
 package swengproject.swengproject;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 import static swengproject.swengproject.R.layout.add_project;
 import static swengproject.swengproject.R.layout.assign_object;
@@ -20,12 +25,19 @@ import static swengproject.swengproject.R.layout.assign_object;
 public class AssignObjectActivity extends AppCompatActivity {
     final int TYPE = 4;
     private ProgressBar pb;
+    EditText datePick;
+    int yearX, dayX, monthX;
+    static final int dialog = 0;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(assign_object);
-
+        final Calendar cal = Calendar.getInstance();
+        yearX = cal.get(Calendar.YEAR);
+        monthX = cal.get(Calendar.MONTH);
+        dayX = cal.get(Calendar.DAY_OF_MONTH);
+        showDialogOnButtonClick();
         pb = (ProgressBar)findViewById(R.id.progressBar);
         pb.setVisibility(View.GONE);
 
@@ -46,6 +58,43 @@ public class AssignObjectActivity extends AppCompatActivity {
             }
         });
     }
+
+    public void showDialogOnButtonClick()
+    {
+        datePick = (EditText) findViewById(R.id.chooseDate);
+        datePick.setOnClickListener( new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                showDialog(dialog);
+            }
+        });
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id)
+    {
+        if(id == dialog)
+        {
+            return new DatePickerDialog(this, dPickerListner, yearX, monthX, dayX);
+        }
+        else
+        {
+            return null;
+        }
+    }
+    private DatePickerDialog.OnDateSetListener dPickerListner
+            = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+            yearX = i;
+            monthX = i1 + 1;
+            dayX = i2;
+            Toast.makeText(AssignObjectActivity.this, dayX + "/" + monthX + "/" + yearX, Toast.LENGTH_LONG).show();
+        }
+    };
+
     public void gather_info() {
 
         ArrayList<String> data = new ArrayList<String>();
@@ -56,7 +105,7 @@ public class AssignObjectActivity extends AppCompatActivity {
 
         EditText fn = (EditText) (findViewById(R.id.objectId));
         String name = fn.getText().toString();
-        EditText ln = (EditText) findViewById(R.id.datePick);
+        EditText ln = (EditText) findViewById(R.id.chooseDate);
         String date = ln.getText().toString();
         meta.add("NAME");
         data.add(name);
