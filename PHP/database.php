c320d414ed6d01fc4d53417030ca6e3d93a9de0d
Rory Devitt <rorytdevitt@gmail.com>
2016-12-06 15:40:07 +0000

    Updated list queries and search_object

--- PHP/database.php
+++ PHP/database.php
@@ -7,7 +7,14 @@
 	$uname="u1429550_sweng";
 	$pwd="sw3ngproject?";
 	$db="db1429550_swengproject";
-
+	
+	define(OBJECT_NOT_FOUND, "0")
+	define(SUCCESS_RESPONSE, "1")
+	define(OBJECT_FOUND, "2")
+	define(LIST_PROJECTS, "3")
+	define(LIST_PERSON, "4")
+	define(DUPLICATE, "5")
+	
 	$con = mysqli_connect($host,$uname,$pwd,$db) or die("connection failed");
 	//
 	// foreach ($_POST as $key => $value) {
@@ -27,9 +34,9 @@
 		break;
 		case 3: search_object();
 		break;
-		case 4: insert_object();
+		case 4: assign_object();
 		break;
-		case 5: assign_object();
+		case 5: 
 		break;
 		case 6: date_objects_list();
 		break;
@@ -57,10 +64,11 @@
 			$sql = "INSERT INTO ProjectGroup (ProjectName,MemberName) VALUES ('$projectName','$indvs[$i]')";
 			$ret = mysqli_query($con,$sql) or die(mysqli_error($con));
 		}
-		echo '1';
+		echo SUCCESS_RESPONSE;
 		mysqli_close($con);
 
 	}
+	
 	function add_individuals($con){
 
 		$name = $_POST["NAME"];
@@ -76,27 +84,82 @@
 			$sql = "INSERT INTO ProjectGroup (ProjectName,MemberName) VALUES ('$projects[$i]','$name')";
 			$ret = mysqli_query($con,$sql) or die(mysqli_error($con));
 		}
-		echo '1';
+		echo SUCCESS_RESPONSE;
 		mysqli_close($con);
 	}
-	function search_object(){
-
+	
+	function search_object($con){
+		$barcode = $_POST['BARCODE_INFO']
+		$sql = "SELECT * FROM Object WHERE Barcode = ('$barcode')";
+		$ret = mysqli_query($con,$sql) or die(mysqli_error($con));
+		
+		if (mysqli_num_rows($ret) == 0) {
+			echo OBJECT_NOT_FOUND;
+		} else {
+			$rows = mysqli_fetch_all($ret);
+			foreach($rows as $row) { 
+				echo $row["ObjectID"] . "#" . $row["Barcode"] . "#" . $row["PersonID"] . "#" . $row["Project"] . "#" . $row["Broken"] . "#";
+			}
+		}
+		mysqli_close($con);
 	}
-	function insert_object(){
-
-	}
+		
 	function assign_object(){
-
+		$name = $_POST['NAME']; //not used
+		$date = $_POST['DATE'];//not used
+		$group = $_POST['GROUP'];
+		$barcode = $_POST['BARCODE'];
+		$individual $_POST['INDIVIDUAL'];
+		$broken = $_POST['DAMAGED'];
+		
+		$sql = "INSERT into Object (Barcode, PersonID, Project, Broken) VALUES ('$barcode', '$indivdual', '$group', '$broken')";
+		$ret = mysqli_query($con,$sql)  or die(mysqli_error($con));
+		echo SUCCESS_RESPONSE;
+		mysqli_close($con)
 	}
-	function date_objects_list(){
-
+	
+	function reclaimed_objects_list(){
+		$sql = "SELECT * FROM Object o LEFT OUTER JOIN Project p ON o.Project = p.Name WHERE p.EndDate < NOW() OR o.Project IS NULL";
+		$ret = mysqli_query($con,$sql) or die(mysqli_error($con));
+		if (mysqli_num_rows($ret) == 0) {
+			echo OBJECT_NOT_FOUND;
+		} else {
+			echo OBJECT_FOUND;
+			$rows = mysqli_fetch_all($ret);
+			foreach($rows as $row) { 
+				echo $row["ObjectID"] . "#" . $row["Barcode"] . "#" . $row["PersonID"] . "#" . $row["Project"] . "#" . $row["Broken"] . "#";
+			}
+		}
+		mysqli_close($con);
 	}
-	function other_objects_list(){
-
+	
+	function broken_objects_list(){
+		$sql = "SELECT * FROM Object o WHERE o.Broken = 1";
+		$ret = mysqli_query($con,$sql) or die(mysqli_error($con));
+		if (mysqli_num_rows($ret) == 0) {
+			echo OBJECT_NOT_FOUND;
+		} else {
+			echo OBJECT_FOUND;
+			$rows = mysqli_fetch_all($ret);
+			foreach($rows as $row) { 
+				echo $row["ObjectID"] . "#" . $row["Barcode"] . "#" . $row["PersonID"] . "#" . $row["Project"] . "#" . $row["Broken"] . "#";
+			}
+		}
+		mysqli_close($con);
 	}
-	function find_object(){
-
+	
+	function attached_objects_list() {
+		$sql = "SELECT * FROM Object o LEFT OUTER JOIN Project p ON o.Project = p.Name WHERE p.EndDate > NOW() AND o.Project IS NOT NULL";
+		$ret = mysqli_query($con,$sql) or die(mysqli_error($con));
+		if (mysqli_num_rows($ret) == 0) {
+			echo OBJECT_NOT_FOUND;
+		} else {
+			echo OBJECT_FOUND;
+			$rows = mysqli_fetch_all($ret);
+			foreach($rows as $row) { 
+				echo $row["ObjectID"] . "#" . $row["Barcode"] . "#" . $row["PersonID"] . "#" . $row["Project"] . "#" . $row["Broken"] . "#";
+			}
+		}
+		mysqli_close($con);
 	}
-
-
 ?>
