4725d086ead12078d4230d6f7b2917b9c4c51a7b
walshd29 <walshd29@tcd.ie>
2016-12-12 18:42:57 +0000

    Proj error not entered

--- SwengProject/app/src/main/java/swengproject/swengproject/AddProjectActivity.java
+++ SwengProject/app/src/main/java/swengproject/swengproject/AddProjectActivity.java
@@ -100,7 +100,7 @@ public class AddProjectActivity extends AppCompatActivity {
             Toast.makeText(AddProjectActivity.this, dayX + "/" + monthX + "/" + yearX, Toast.LENGTH_LONG).show();
         }
     };
-    public void gather_info() {
+    public boolean gather_info() {
 
         ArrayList<String> data = new ArrayList<String>();
         ArrayList<String> meta = new ArrayList<String>();
@@ -108,6 +108,10 @@ public class AddProjectActivity extends AppCompatActivity {
         data.add(TYPE+"");
 
         EditText n = (EditText) (findViewById(R.id.editText));
+        if( n.getText().toString().length() == 0 ) {
+            n.setError("Project name is required!");
+            return false;
+        }
         String name = n.getText().toString();
         meta.add("NAME");
         data.add(name);
@@ -139,6 +143,7 @@ public class AddProjectActivity extends AppCompatActivity {
         passData.putExtra("PREVIOUS_ACTIVITY",AddProjectActivity.class);
 
         startActivityForResult(passData,0);
+        return true;
 
     }
     @Override
